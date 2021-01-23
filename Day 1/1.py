9f012be06903ca461889a2f60087f7a90dcd2618
Raj <raj@pop-os.localdomain>
2021-01-23 16:03:14 +0530

    1 Day solutions

---
+++ Day 1/1.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+my_i=int(input())
+my_d=float(input())
+my_s=input()
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i+my_i)
+# Print the sum of the double variables on a new line.
+print(d+my_d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+my_s)
